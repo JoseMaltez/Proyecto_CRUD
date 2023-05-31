879c12cf26c752da6913cf9ddacabc2ddc5a0869
JoseMaltez <jmaltezv@miumg.edu.gt>
2023-05-30 19:09:35 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto_CRUD/Form1.h
@@ -0,0 +1,175 @@
+#pragma once
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
+
+	// Aqui se crea la clase
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: String^ connectionString = "datasource=localhost; username=root; password=localhost; database=proyectocrud;";
+	private: MySqlConnection^ conn = gcnew MySqlConnection(connectionString);
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ txt_nombre;
+	private: System::Windows::Forms::TextBox^ txt_contra;
+
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->txt_nombre = (gcnew System::Windows::Forms::TextBox());
+			this->txt_contra = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(79, 213);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(115, 36);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Login";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// txt_nombre
+			// 
+			this->txt_nombre->Location = System::Drawing::Point(46, 42);
+			this->txt_nombre->Multiline = true;
+			this->txt_nombre->Name = L"txt_nombre";
+			this->txt_nombre->Size = System::Drawing::Size(193, 38);
+			this->txt_nombre->TabIndex = 1;
+			this->txt_nombre->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->txt_nombre->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// txt_contra
+			// 
+			this->txt_contra->Location = System::Drawing::Point(46, 120);
+			this->txt_contra->Multiline = true;
+			this->txt_contra->Name = L"txt_contra";
+			this->txt_contra->PasswordChar = '*';
+			this->txt_contra->Size = System::Drawing::Size(193, 38);
+			this->txt_contra->TabIndex = 2;
+			this->txt_contra->TextAlign = System::Windows::Forms::HorizontalAlignment::Center;
+			this->txt_contra->TextChanged += gcnew System::EventHandler(this, &Form1::textBox2_TextChanged);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(43, 26);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(43, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Usuario";
+			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(43, 104);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(61, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Contraseña";
+			this->label2->Click += gcnew System::EventHandler(this, &Form1::label2_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->txt_contra);
+			this->Controls->Add(this->txt_nombre);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ sql = "select * from login where Usuario= '" + txt_nombre->Text + "' and Contra = '" + txt_contra->Text + "'";
+	MySqlCommand^ cursor = gcnew MySqlCommand(sql, conn);
+	MySqlDataReader^ dataReader;
+
+	try {
+		this->conn->Open();
+		dataReader = cursor->ExecuteReader();
+		if (dataReader->Read())
+		{
+			MessageBox::Show(L"Bienvenido " + txt_nombre->Text);
+
+		}
+		else
+		{
+			MessageBox::Show(L"Usuario incorrecto");
+			this->conn->Close();
+		}
+	}
+	catch(Exception^x){
+		MessageBox::Show(x->Message);
+
+	}
+
+}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
